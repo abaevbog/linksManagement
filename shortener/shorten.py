ca3176fe2ebfc339e798d26baf6f68d26effcc62
Bogdan Abaev <abaevbog@grinnell.edu>
2020-08-25 11:29:24 -0400

    minor fixes

--- shortener/shorten.py
+++ shortener/shorten.py
@@ -8,19 +8,20 @@ table = dynamodb.Table('linkStorage')
 
 def handler(event,context):
     params = json.loads(event['body'])['parameters']
+    params = json.loads(params)
+    category = event['requestContext']['domainPrefix']
     output = {}
     for param in params:
         long_link = param['link']
         lead_id = param['lead_id']
-        personal_touch = param['personal_touch']
-        category = param['category'] #form or int, used as prefix for domain
+        personal_touch = param['personal_touch'].replace(" ", "%20")
         tag = param['tag']
         uid = randint(0,10000)
         short_link = f"{category}.{personal_touch}-{uid}"
         table.put_item(
         Item={
                 'link': short_link,
-                'long_link': long_link,
+                'long_link': long_link.replace(" ", "%20"),
                 'lead_id': lead_id,
                 'tag': tag
             }
