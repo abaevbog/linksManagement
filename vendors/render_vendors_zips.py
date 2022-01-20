57f1a6c0d0aba8d98e314c67f0f87f9826b8f1d9
Bogdan Abaev <abaevbog@gmail.com>
2022-01-20 13:25:47 -0500

    vendors service area

---
+++ vendors/render_vendors_zips.py
@@ -0,0 +1,46 @@
+import json
+import os
+from jinja2 import Environment, FileSystemLoader
+import requests
+import ast
+import traceback
+import urllib
+
+
+
+
+def handler(event,context):
+    try:
+        trade = event["queryStringParameters"].get('trade')
+        
+        r = requests.get(
+            "https://api.airtable.com/v0/appiScywNMqBk3x9e/Directory",
+        params= urllib.parse.urlencode({
+            'maxRecords' : '100',
+            'view' : "Vendors_Service_Area",
+            "filterByFormula" : f"FIND('{trade}', {'{Division and Trade}'})",
+            "fields[0]" : "Full Name", 
+            "fields[1]" : "Zip Codes Serviced",
+            "fields[2]" : "Category",
+            "fields[3]" : "Division and Trade",
+        },quote_via=urllib.parse.quote),
+        headers={
+            "Authorization": "Bearer keyfcdTfG74vVDNCo"
+        })
+        
+        result = json.loads(r.content)
+        
+        env = Environment(loader=FileSystemLoader('./vendors'), trim_blocks=True, lstrip_blocks=True)
+        template = env.get_template(f"zips.html")
+        page = template.render(data= result)
+    except Exception as e:
+        print("Error!!")
+        print(e)
+        traceback.print_exc() 
+        page = "<html>Error. Something broke, please yell at bogdan.</html>" 
+    finally:
+        return {
+                "statusCode":200,
+                "headers": {'Content-Type': 'text/html'},
+                "body":page
+                }
